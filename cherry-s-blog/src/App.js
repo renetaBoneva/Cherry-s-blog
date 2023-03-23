caea462fde08bde1506c67cb8d9bbae2783547bc
renetaBoneva <reneta.boneva@abv.bg>
2023-03-23 23:44:56 +0200

    Creating json store for recipes

--- cherry-s-blog/src/App.js
+++ cherry-s-blog/src/App.js
@@ -15,17 +15,7 @@ import { RecipeEdit } from "./components/Recipe/RecipeEdit";
 import { UserProfileEdit } from "./components/User/UserProfileEdit";
 
 function App() {
-  const [recipesData, setRecipesData ] = useState([{
-    _id: '1',
-    title: "Cherry cake",
-    img: "/img/cherryCake.jpg",
-    ingredients: "200g soft unsalted butter, plus extra for the tin\n200g golden caster sugar\n4 eggs\n2 tsp vanilla extract\n1 tsp almond extract (optional)\n75g glacé cherries, patted dry if in syrup, then quartered\n175g self-raising flour\n50g ground almonds\n½ tsp baking powder\n",
-    method: "STEP 1\nHeat the oven to 180C/160C fan/gas 4. Butter a 900g loaf tin and line with baking parchment. Beat the butter and sugar together until pale and fluffy, then beat in the eggs one at a time, followed by the vanilla and almond extract (if using).\nSTEP 2\nToss the cherries with 2 tbsp of the flour in a bowl. Tip the rest of the flour, the ground almonds, baking powder and a pinch of salt into the bowl with the sugar and eggs, and mix until just combined. Fold in the cherries and any remaining flour in the bowl. Spoon into the prepared tin and smooth over the top.\nSTEP 3\nBake for 50-55 mins, or until a skewer inserted in the middle comes out clean. Leave to cool for 10 mins in the tin, then transfer to a wire rack to cool completely. Serve as it is, or mix the icing sugar with the extract and enough milk to make a thick but pourable consistency. Drizzle over the loaf cake, and scatter over the flaked almonds to serve.\n",
-    comments: [{
-        name: "Lili Semizova",
-        value: "That’s my favorite cake to cook! "
-    }]
-  }])
+  const [recipesData, setRecipesData ] = useState()
   return (
     <>
       <Navigation />
