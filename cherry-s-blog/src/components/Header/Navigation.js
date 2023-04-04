803ebebf8d135faeb5e195157605b90ad239f3e1
renetaBoneva <reneta.boneva@abv.bg>
2023-04-04 21:52:25 +0300

    Updating navigation

--- cherry-s-blog/src/components/Header/Navigation.js
+++ cherry-s-blog/src/components/Header/Navigation.js
@@ -1,7 +1,12 @@
 import { Link } from "react-router-dom";
 
+import { useContext } from "react";
+import { AuthContext } from "../../contexts/AuthContext";
+
 
 export function Navigation() {
+    const { isAuthenticated, userId } = useContext(AuthContext);
+
     return (
         <header>
             <nav className="wrapper">
@@ -14,13 +19,20 @@ export function Navigation() {
                 <div>
                     <ul id="navUL">
                         <li><Link to="/catalog">Catalog</Link></li>
-                        {/* <!-- Guest menu --> */}
-                        <li><Link to="/login">Login</Link></li>
-                        <li><Link to="/register">Register</Link></li>
-                        {/* <!-- Logged in user menu --> */}
-                        <li><Link to="/addRecipe">Add recipe</Link></li>
-                        <li><Link to="/users/:id/profile">My profile</Link></li>
-                        <li><Link to="/logout">Logout</Link></li>
+                        {isAuthenticated ? (
+                            <>
+                            {/* <!-- Logged in user menu --> */}
+                            <li><Link to="/addRecipe">Add recipe</Link></li>
+                            <li><Link to={`/users/${userId}/profile`}>My profile</Link></li>
+                            <li><Link to="/logout">Logout</Link></li>
+                            </>
+                        ) : ( 
+                            <>                          
+                            {/* <!-- Guest menu --> */}
+                            <li><Link to="/login">Login</Link></li>
+                            <li><Link to="/register">Register</Link></li>
+                            </> 
+                        )}
                     </ul>
                 </div>
             </nav>
