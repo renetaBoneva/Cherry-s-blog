e11dcd1922032045eb8f3f3217cf6c60eaf13795
renetaBoneva <reneta.boneva@abv.bg>
2023-04-04 17:34:59 +0300

    Creating requester

---
+++ cherry-s-blog/src/services/requester.js
@@ -0,0 +1,49 @@
+
+
+async function requester(method, token, url , data) {
+    let options = {};
+    if(method !== "GET") {
+        options.method = method;
+        if(data) {
+            options.headers = {
+                "Content-Type": "application/json"
+            }
+            options.body = JSON.stringify(data);
+        }
+    }
+
+    if(token) {
+        options.headers = {
+            ...options.headers,
+            "X-Authorization": token
+        }
+    }
+
+    const res = await fetch(url, options);
+    if(res.status === 204) {
+        return {}
+    }
+
+    const result = await res.json();
+    if(!res.ok) {
+        throw result;
+    }
+    
+    return result;
+}
+
+export function requestFactory() {
+    if(!token) {
+        const serializedToken = localStorage.getItem('auth');
+        if(serializedToken) {
+            token = JSON.parse(serializedToken);
+        }
+    }
+
+    return {
+        get: requester(null, "GET", token),
+        post: requester(null, "POST", token),
+        put: requester(null, "PUT", token),
+        del: requester(null, "DELETE", token)
+    }
+}
