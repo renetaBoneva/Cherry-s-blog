5459fc391bb45cf758e54877480b4905e91dba5f
renetaBoneva <reneta.boneva@abv.bg>
2023-04-04 17:34:50 +0300

    Updating component to use useForm hook

--- cherry-s-blog/src/components/Recipe/AddRecipePage.js
+++ cherry-s-blog/src/components/Recipe/AddRecipePage.js
@@ -1,27 +1,13 @@
-import { useState } from "react";
+import { useForm } from "../../hooks/useForm";
 
-export function AddRecipePage({
-    setRecipesData
-}) {
-    const [formData, setFormData] = useState({
-        title: "",
-        imageUrl: "",
-        ingredients: "",
-        method: ""
-    });
-
+export function AddRecipePage() {
     document.body.style.backgroundImage = `url('/img/greenPinkBgr.png')`;
-    
-    function onCreateFormChange(e) {
-        setFormData(state => ({ ...state, [e.target.name]: e.target.value }))
-    }
-    
-    function onCreateRecipeSubmit(e) {
-        e.preventDefault();
-        
-        console.log('//TODO: Send post request to the server');
-        setRecipesData(state => [...state, formData]);
-    }
+    const {values, chnageValues, onSubmitClick} = useForm({
+            title: "",
+            imageUrl: "",
+            ingredients: "",
+            method: ""
+        }, (values) => console.log(values));
 
     return (
         <main style={{
@@ -30,18 +16,40 @@ export function AddRecipePage({
         }}>
 
             <section id="addRecipeSection" className="wrapper">
-                <form onSubmit={onCreateRecipeSubmit}>
+                <form method="POST" onSubmit={onSubmitClick}>
                     <h2>Add recipe</h2>
                     <label htmlFor="title" >Title: </label>
-                    <input type="text" name="title" value={formData.title} onChange={onCreateFormChange} />
+                    <input
+                        type="text"
+                        name="title"
+                        value={values.title}
+                        onChange={chnageValues}
+                    />
                     <label htmlFor="imageUrl">Image: </label>
-                    <input type="text" name="imageUrl" value={formData.imageUrl} onChange={onCreateFormChange} />
+                    <input
+                        type="text"
+                        name="imageUrl"
+                        value={values.imageUrl}
+                        onChange={chnageValues}
+                    />
                     <label htmlFor="ingredients">Ingredients: </label>
-                    <textarea type="text" rows="5" name="ingredients" value={formData.ingredients} onChange={onCreateFormChange}></textarea>
-                    <label htmlFor="method">Method: </label>
-                    <textarea type="text" rows="5" name="method" value={formData.method} onChange={onCreateFormChange}></textarea>
+                    <textarea
+                        type="text"
+                        rows="5"
+                        name="ingredients"
+                        value={values.ingredients}
+                        onChange={chnageValues}
+                    ></textarea>
+                   <label htmlFor="method">Method: </label>
+                    <textarea
+                        type="text"
+                        rows="5"
+                        name="method"
+                        value={values.method}
+                        onChange={chnageValues}
+                    ></textarea>
 
-                    <input type="submit" value="SUBMIT" />
+                    <input type="submit" value="ADD" />
                 </form>
             </section>
 
